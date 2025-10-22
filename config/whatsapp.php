a07953422664cc1cc130e12a63c98126202dac6e
Athul Raj <mail@athulraj.com>
2025-10-23 03:05:29 +0530

    sendFreeText

--- config/whatsapp.php
+++ config/whatsapp.php
@@ -6,6 +6,7 @@ return [
         'common' => [
             'webhook_secret' => env('WHATSAPP_COMMON_APP_WEBHOOK_SECRET', '01dfb5bb-ce14-4fe7-8507-c23c6d178256'),
             'api_key' => env('WHATSAPP_COMMON_APP_API_KEY', ''),
+            'number_id' => env('WHATSAPP_COMMON_APP_NUMBER_ID', ''),
         ],
     ],
 ];
--- src/Whatsapp.php
+++ src/Whatsapp.php
@@ -2,17 +2,37 @@
 
 namespace ScaleXY\Whatsapp;
 
+use Illuminate\Support\Facades\Http;
+
 class Whatsapp
 {
     protected $app_name;
 
-    // protected $webhook_secret;
+    protected $number_id;
+
     protected $api_key;
+
+    protected $client;
 
     public function __construct($config = [])
     {
         $this->app_name = $config['app_name'] ?? config('whatsapp.apps.'.config('whatsapp.default_app').'.app_name');
         // $this->webhook_secret = $config['webhook_secret'] ?? config('whatsapp.apps.' . $this->app_name . '.webhook_secret');
         $this->api_key = $config['api_key'] ?? config('whatsapp.apps.'.$this->app_name.'.api_key');
+        $this->number_id = $config['number_id'] ?? config('whatsapp.apps.'.$this->app_name.'.number_id');
+        $this->client = Http::withToken($this->api_key);
+    }
+
+    public function sendFreeText($number, $message)
+    {
+        return $this->client->post('https://graph.facebook.com/v22.0/'.$this->number_id.'/messages', [
+            'messaging_product' => 'whatsapp',
+            'recipient_type' => 'individual',
+            'to' => $number,
+            'text' => [
+                'preview_url' => false,
+                'body' => $message,
+            ],
+        ]);
     }
 }
